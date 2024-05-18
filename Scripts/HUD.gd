46ebc8f933ad124d63c6e2eb089ebdd447c7873f
Ash <68016221+AshOnDiscord@users.noreply.github.com>
2024-05-18 11:26:11 -0400

    Fix hud

--- Scripts/HUD.gd
+++ Scripts/HUD.gd
@@ -1,13 +1,13 @@
-extends CanvasLayer
+extends Control
 
+@onready var label = $Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-  self.show()
   pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-  self.text = $EnemyManager.gold
+  label.text = "Gold:" +str(%EnemyManager.gold)
   pass
